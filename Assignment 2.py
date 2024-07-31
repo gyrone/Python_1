59fa40513a5a56d38d9c81edb17eb7a777d2c554
gyrone <44639593+gyrone@users.noreply.github.com>
2024-07-31 07:13:10 -0400

    Add files via upload
    Adding my first two assignments

---
+++ Assignment 2.py
@@ -0,0 +1,5 @@
+length = float(input("Length: "))
+width = float(input("width: "))
+Area = length * width
+
+print("Area:",Area)
---
+++ assignment 1.py
@@ -0,0 +1,6 @@
+Principal = float(input("Principal: "))
+Rate = float(input("Rate: "))
+Time = float(input("time: "))
+SimpleInterest = (Principal * Rate * Time) / 100.
+
+print("SimpleInterest: {0}".format(SimpleInterest))
